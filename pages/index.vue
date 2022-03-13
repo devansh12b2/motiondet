7d4e1f9b4e59efea037e4dab02309d14e059bcda
Devansh Mishra <79010144+devansh12b2@users.noreply.github.com>
2022-03-13 17:19:19 +0530

    motion detector

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="main-heading">
+      <h1>MOTION DETECTOR</h1>
+    </div>
+    <MotionDetect />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style scoped>
+.main-heading {
+  align-content: center;
+  text-align: center;
+}
+</style>
